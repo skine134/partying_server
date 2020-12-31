b9126203e06f1635eac1a421beea7dba2214d4d3
skine134 <skine134@naver.com>
2020-12-31 22:02:23 +0900

    info  파일 추가

---
+++ Info.cs
@@ -0,0 +1,42 @@
+using Newtonsoft.Json.Linq;
+
+namespace patting_server
+{
+    public class Info
+    {
+        private JObject usersInfo = new JObject();
+        private JObject aiInfo = new JObject();
+
+        public Map UsersInfo
+        {
+                get
+                {
+                        return usersInfo;
+                }
+
+            
+                set
+                {
+                lock(usersInfo){
+                        usersInfo = value;
+                }
+                }
+        }
+
+        public Map AiInfo
+        {
+                get
+                {
+                        return aiInfo;
+                }
+
+            
+                set
+                {
+                lock(aiInfo){
+                        aiInfo = value;
+                }
+                }
+        }
+    }
+}
