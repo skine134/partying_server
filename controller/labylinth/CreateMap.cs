2e90da7a55f630ca649c22ac5729e2651992cdbf
skine134 <skine134@naver.com>
2021-01-21 03:33:43 +0900

    CreateMap 오류 수정[맵이 정상적으로 생성되지 않는 문제] 및 테스트 완료

--- controller/labylinth/CreateMap.cs
+++ controller/labylinth/CreateMap.cs
@@ -14,36 +14,36 @@ namespace patting_server.controller.labylinth
         public const int RIGHT = 1;
         public const int UP = 2;
         public const int DOWN = 3;
-        public int Rows; //배열의 행에 해당함
-        public int Columns; // 배열의 열에 해당함
+        public int columns; //배열의 행에 해당함
+        public int rows; // 배열의 열에 해당함
         private static Random random = new Random();
 
         private int[,,] grid; //미로를 만들기 위한 격자 생성 {left,right,up,down}
         private string[,] Spawn; //유닛 오브젝트의 위치를 지정하기 위한 배열 생성
-        private int initRow; // 행에 대한 미로찾기를 위한 처음의 시작값
-        private int initColumn;  // 열에 대한 미로찾기를 위한 처음의 시작값
+        private int initcolumn; // 행에 대한 미로찾기를 위한 처음의 시작값
+        private int initrow;  // 열에 대한 미로찾기를 위한 처음의 시작값
         private int trapCount; //함정 오브젝트의 수량 제한
         private HashSet<string> visitCell = new HashSet<string>();
-        public CreateMap(int Rows, int Columns)
+        public CreateMap(int columns, int rows)
         {
-            this.Rows = Rows;
-            this.Columns = Columns;
-            this.initRow = random.Next(0, Rows);
-            this.initColumn = random.Next(0, Columns);
-            this.trapCount = (int)(Rows * Columns * 4 / 10);
-            CreateGrid(this.Rows, this.Columns);
+            this.columns = columns;
+            this.rows = rows;
+            this.initcolumn = random.Next(0, columns);
+            this.initrow = random.Next(0, rows);
+            this.trapCount = (int)(columns * rows * 4 / 10);
+            CreateGrid(this.columns, this.rows);
             HuntAndKill();
             SetTrap(trapCount);
             Connection.Send(this.ToString());
         }
         public CreateMap(int size) : this(size, size) { }
-        void CreateGrid(int rows, int columns) // 그리드를 쉽게 호출하기 위해 함수로 정의
+        void CreateGrid(int columns, int rows) // 그리드를 쉽게 호출하기 위해 함수로 정의
         {
-            grid = new int[rows, columns, 4]; //행과 열을 설정하여 미로를 위한 격자를 초기화함
-            Spawn = new string[rows, columns];
-            for (int i = 0; i < rows; i++)
+            grid = new int[columns, rows, 4]; //행과 열을 설정하여 미로를 위한 격자를 초기화함
+            Spawn = new string[columns, rows];
+            for (int i = 0; i < columns; i++)
             {
-                for (int j = 0; j < columns; j++)
+                for (int j = 0; j < rows; j++)
                 {
                     for (int k = 0; k < grid.GetLength(2); k++)
                         grid[i, j, k] = 0;
@@ -60,36 +60,37 @@ namespace patting_server.controller.labylinth
 
         void HuntAndKill()
         {
-            Walk(initRow, initColumn);
+            Walk(initcolumn, initrow);
             while (!Hunt()) ;
         }
 
-        void Walk(int row, int column)
+        void Walk(int column, int row)
         {
-            visitCell.Add(PointToString(row, column)); // 현재 행과 열에 대해 방문한 grid를 저장한다. / 랜덤한 방향으로 나아가기 위한 첫번째 cell을 방문한 것으로 처리한다.
-            int[] notVisitedArray = GetNotVisitedNeighbors(row, column); // 방문하지 않은 grid들을 가져온다.
+            visitCell.Add(PointToString(column, row)); // 현재 행과 열에 대해 방문한 grid를 저장한다. / 랜덤한 방향으로 나아가기 위한 첫번째 cell을 방문한 것으로 처리한다.
+            int[] notVisitedArray = GetNotVisitedNeighbors(column, row); // 방문하지 않은 grid들을 가져온다.
             if (notVisitedArray.Length <= 0)
                 return;
             int direction = random.Next(0, notVisitedArray.Length); // 방문하지 않은 grid들(notVisitedArray) 중에서 
-            DestroyWall(row, column, notVisitedArray[direction]);
+            DestroyWall(column, row, notVisitedArray[direction]);
+            direction = notVisitedArray[direction];
             // 위쪽방향 확인
-            switch (notVisitedArray[direction])
+            switch (direction)
             {
                 case LEFT:
-                    DestroyWall(row - 1, column, RIGHT);
-                    Walk(row - 1, column);
+                    DestroyWall(column, row - 1, RIGHT);
+                    Walk(column, row - 1);
                     break;
                 case RIGHT:
-                    DestroyWall(row + 1, column, LEFT);
-                    Walk(row + 1, column);
+                    DestroyWall(column, row + 1, LEFT);
+                    Walk(column, row + 1);
                     break;
                 case UP:
-                    DestroyWall(row, column - 1, DOWN);
-                    Walk(row, column - 1);
+                    DestroyWall(column - 1, row, DOWN);
+                    Walk(column - 1, row);
                     break;
                 case DOWN:
-                    DestroyWall(row, column + 1, UP);
-                    Walk(row, column + 1);
+                    DestroyWall(column + 1, row, UP);
+                    Walk(column + 1, row);
                     break;
                 default:
                     break;
@@ -101,57 +102,57 @@ namespace patting_server.controller.labylinth
             while (visitCellEnum.MoveNext())
             {
 
-                string[] rowAndCol = visitCellEnum.Current.Split(",");
-                int row = int.Parse(rowAndCol[0]);
-                int column = int.Parse(rowAndCol[1]);
-                if (GetNotVisitedNeighbors(row, column).Length > 0) //해당 배열의 방문여부 확인 후 방문 했다면  계속 방문하지 않았다면 walk 실행
+                string[] columnAndCol = visitCellEnum.Current.Split(",");
+                int column = int.Parse(columnAndCol[0]);
+                int row = int.Parse(columnAndCol[1]);
+                if (GetNotVisitedNeighbors(column, row).Length > 0) //해당 배열의 방문여부 확인 후 방문 했다면  계속 방문하지 않았다면 walk 실행
                 {
-                    Walk(row, column);
+                    Walk(column, row);
                     return false;
                 }
             }
 
             return true;
         }
-        void DestroyWall(int row, int column, int direction) //벽제거(사냥)을 위한 함수
+        void DestroyWall(int column, int row, int direction) //벽제거(사냥)을 위한 함수
         {
             switch (direction)
             {
                 case LEFT:
-                    grid[row, column, LEFT] = 0;
+                    grid[column, row, LEFT] = 0;
                     break;
                 case RIGHT:
-                    grid[row, column, RIGHT] = 0;
+                    grid[column, row, RIGHT] = 0;
                     break;
                 case UP:
-                    grid[row, column, UP] = 0;
+                    grid[column, row, UP] = 0;
                     break;
                 case DOWN:
-                    grid[row, column, DOWN] = 0;
+                    grid[column, row, DOWN] = 0;
                     break;
             }
 
         }
-        public bool IsVisited(int row, int column)
+        public bool IsVisited(int column, int row)
         {
 
-            if ((0 > row || row >= Rows) || (0 > column || column >= Columns))
+            if ((0 > column || column >= columns) || (0 > row || row >= rows))
                 return true;
-            return visitCell.Contains(PointToString(row, column));
+            return visitCell.Contains(PointToString(column, row));
         }
 
-        public bool IsVisitedNeighbors(int currentRow, int currentColumn)
+        public bool IsVisitedNeighbors(int currentcolumn, int currentrow)
         {
             {
-                int rowSub = 1;
                 int columnSub = 1;
+                int rowSub = 1;
                 for (int i = 0; i < 2; i++)
                 {
-                    rowSub *= -1;
+                    columnSub *= -1;
                     for (int j = 0; j < 2; j++)
                     {
-                        columnSub *= -1;
-                        if (!IsVisited(currentRow + rowSub, currentColumn + columnSub))
+                        rowSub *= -1;
+                        if (!IsVisited(currentcolumn + columnSub, currentrow + rowSub))
                         {
                             return false;
                         }
@@ -161,50 +162,49 @@ namespace patting_server.controller.labylinth
             return true;
         }
 
-        public int[] GetNotVisitedNeighbors(int currentRow, int currentColumn)
+        public int[] GetNotVisitedNeighbors(int currentcolumn, int currentrow)
         {
             List<int> notVisitedArray = new List<int>();
-            if (!IsVisited(currentRow - 1, currentColumn))
+            if (!IsVisited(currentcolumn, currentrow - 1))
                 notVisitedArray.Add(LEFT);
-            if (!IsVisited(currentRow + 1, currentColumn))
+            if (!IsVisited(currentcolumn, currentrow + 1))
                 notVisitedArray.Add(RIGHT);
-            if (!IsVisited(currentRow, currentColumn - 1))
+            if (!IsVisited(currentcolumn- 1, currentrow ))
                 notVisitedArray.Add(UP);
-            if (!IsVisited(currentRow, currentColumn + 1))
+            if (!IsVisited(currentcolumn + 1, currentrow))
                 notVisitedArray.Add(DOWN);
             return notVisitedArray.ToArray();
         }
-        string PointToString(int row, int column)
+        string PointToString(int column, int row)
         {
-            return String.Format("{0},{1}", row, column);
+            return String.Format("{0},{1}", column, row);
         }
         void SetTrap(int count)
         {
             for (int i = 0; i < count; i++)
             {
                 // 함정을 랜덤으로 생성하는 역할
-                int row = random.Next(1, (Columns - 2));
-                int column = random.Next(1, (Rows - 2));
+                int column = random.Next(1, (rows - 2));
+                int row = random.Next(1, (columns - 2));
                 int Rand = random.Next(0, 2);
                 //Player Respon구간은 각 모서리의 2*2구간만큼 랜덤 리스폰 구상중
                 //AI Respon구간은 정 중앙의 3*3구간의 랜덤 리스폰 구상중
-                if (!Spawn[row, column].Equals("")) // ResponCheck를 통해 해당 배열구간에 다른 오브젝트의 여부를 확인 추후(AI,Player)를 추가하여 함정 설치
+                if (!Spawn[column, row].Equals("")) // ResponCheck를 통해 해당 배열구간에 다른 오브젝트의 여부를 확인 추후(AI,Player)를 추가하여 함정 설치
                 {
                     if (Rand == 0)
                     { //가시함정 오브젝트
-                        Spawn[row, column] = "SpikeTrap";
+                        Spawn[column, row] = "SpikeTrap";
                     }
                     if (Rand == 1)
                     {//바닥함정 오브젝트
-                        Spawn[row, column] = "HoleTrap";
+                        Spawn[column, row] = "HoleTrap";
                     }
                 }
             }
         }
         public override string ToString()
         {
-            //TODO clearItem 수정 필요, patrolPoint 추가 필요.
-            return JsonConvert.SerializeObject(new { labylinthArray = grid, trap = Spawn, clearItem = new { seat = new { row = 12, col = 4 } } });
+            return JsonConvert.SerializeObject(new { labylinthArray = grid, trap = Spawn, clearItem = new { seat = new { column = 12, col = 4 } } });
         }
     }
 }
