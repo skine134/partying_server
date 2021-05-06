5b52cf3b10736adfb83c99ca3b722a940f47839d
DESKTOP-5B9VU1L\skine <skine134@naver.com>
2021-05-06 20:25:55 +0900

    1스테이지 타이머 적용.

--- controller/common/SyncStart.cs
+++ controller/common/SyncStart.cs
@@ -13,7 +13,11 @@ namespace partying_server.controller
                 return;
                 Info.SyncCount.Clear();
                 var datetime= System.DateTime.Now.AddSeconds(5);
-                Connection.SendAll(Common.GetResponseFormat("SyncStart", new {startTime = Common.ConvertToUnixTimestamp(datetime)}));
+            var data = JObject.FromObject(new {startTime = Common.ConvertToUnixTimestamp(datetime)});
+            if(Info.currentStage==1)
+                data["finishTime"] = Common.ConvertToUnixTimestamp(System.DateTime.Now.AddMinutes(2));
+            
+            Connection.SendAll(Common.GetResponseFormat("SyncStart", data));
             if(Info.currentStage==2)
             {
                 AsyncTimer timeEvent = new AsyncTimer(1f);
